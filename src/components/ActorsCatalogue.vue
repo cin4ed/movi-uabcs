71d61125b725775a6613a6d8d798a47167f84261
Rafael-cmd-html <armandocastillejos086@gmail.com>
2023-10-11 06:38:48 -0700

    Lista de actores
    Extracción de los actores, por lo pronto nombres de lo más populares

---
+++ src/components/ActorsCatalogue.vue
@@ -0,0 +1,78 @@
+<template>
+    <div id='app'>
+
+
+        <div class="hero is-white is-gradient is-bold">
+
+            <div class="hero-body">
+
+                <h1 class="title">
+
+                    <span class="has-text-success">Actores</span>
+                    <span class="subtitle">Nombres</span>
+
+
+                </h1>
+                <button class="button is-success is-rounded" v-on:click="fetch">Consultar Actores</button>
+
+
+            </div>
+
+        </div>
+
+        <h1 class="title">Lista de actores</h1>
+
+
+        <div v-for="character of characters" v-bind:key="character.id">
+
+           <p>{{ character.name}}</p>
+
+        </div>
+    </div>
+</template>
+<script>
+
+    import { createApp } from 'vue'
+    import axios from 'axios'
+    import VueAxios from 'vue-axios'
+
+    const app = createApp()
+
+    app.use(VueAxios, axios)
+    export default{
+
+        name:"Actor List",
+        data: function (){
+
+            return {
+
+                characters: []
+
+            }
+
+        },
+        methods:{
+
+            fetch(){
+
+                let result= axios.get("https://api.themoviedb.org/3/person/popular?api_key=6a71a113dddd8d476e8b8e07db83bb9d").then((res) => {
+
+                    this.characters=res.data.results
+                    console.log(this.characters)
+
+                })
+                .catch((err) =>{
+
+                    console.log(err)
+
+                })
+
+                console.log("Hola mundo")
+
+            }
+
+        }
+    }
+
+
+</script>
--- src/main.js
+++ src/main.js
@@ -7,14 +7,16 @@ import App from './App.vue';
 // Route components
 import Index from './components/MovieCatalogue.vue';
 import Login from './components/Login.vue';
-import Serie from './components/Serie.vue';
+//import Serie from './components/Serie.vue';
+import Actors from './components/ActorsCatalogue.vue';
 
 // Define routes
 const routes = [
     { path: '/', component: Login },
     { path: '/login', component: Login },
     { path: '/index/', component: Index },
-    { path: '/serie/:id', component: Serie },
+    //{ path: '/serie/:id', component: Serie },
+    { path: '/actors', component: Actors},
 ];
 
 // Create router instance and pass the routes
