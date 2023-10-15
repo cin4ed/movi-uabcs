9f585bfb01c707c68a189d9fa8a94b3d3788f8a0
Rafael-cmd-html <armandocastillejos086@gmail.com>
2023-10-15 09:51:45 -0700

    Previous y next para página de actores

--- src/components/ActorsCatalogue.vue
+++ src/components/ActorsCatalogue.vue
@@ -8,10 +8,20 @@
             <ActorsItem :key="actor.id" v-for="actor in this.actors.results" :actor="actor"/> 
         </div>
 
+        <!-- Se crearon los "botones" de previous y next para cambiar de página de actores -->
+        <div class="more_actors">
+
+            <a href="" v-on:click.prevent="previous"> Previous </a>
+            <a href="" v-on:click.prevent="next"> Next </a>
+
+        </div>
+
     </div>
 
 </template>
 <script>
+// Cree una variable para poder cambiar de página de actores
+let currentPage=1;
 import ActorsItem from "../components/ActorsItem.vue"
 import ActorsDetails from "../components/ActorsDetails.vue"
 export default{
@@ -31,29 +41,53 @@ export default{
 
     },
     mounted(){
-
-        const options = {
-            method: 'GET',
-            headers: {
-
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTcxYTExM2RkZGQ4ZDQ3NmU4YjhlMDdkYjgzYmI5ZCIsInN1YiI6IjY1MTlkOTY1MDcyMTY2MDEzOWM1ZDQ4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fvwRaFzNROAhNcfeY1qE_fR1eUi4rKTly4QCrm8u-C4',
-                "content-type": "application/json"
-            }
-        };
-
-        fetch('https://api.themoviedb.org/3/person/popular?language=en-US&page=1', options)
-        .then(response => response.json()) 
-        .then(json => this.actors = json)
-        .catch(err => console.error(err));
-
+        this.fetchActors(currentPage);
     },
     methods: {
+
+        // La petición la hice un método para poder cambiar de página
+        fetchActors(page){
+            const options = {
+                method: 'GET',
+                headers: {
+
+                    accept: 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTcxYTExM2RkZGQ4ZDQ3NmU4YjhlMDdkYjgzYmI5ZCIsInN1YiI6IjY1MTlkOTY1MDcyMTY2MDEzOWM1ZDQ4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fvwRaFzNROAhNcfeY1qE_fR1eUi4rKTly4QCrm8u-C4',
+                    "content-type": "application/json"
+                }
+            };
+            
+            // Aqui en vez de pasarle el número de pagina, le pasamos nuestra variable directamente
+            fetch('https://api.themoviedb.org/3/person/popular?language=en-US&page='+currentPage, options)
+            .then(response => response.json()) 
+            .then(json => this.actors = json)
+            .catch(err => console.error(err));
+        },
+        next(){
+
+            currentPage+=1;
+            this.fetchActors(currentPage);
+        },
+        previous(){
+
+            // Para verificar que la página no llegue a un numero negativo, se hace la validación
+            if(currentPage==1){
+
+                alert("Estás en la primera página!")
+
+            }else{
+                
+                currentPage-=1;
+                this.fetchActors(currentPage);
+
+            }
+
+        }
     },
 
 }
 </script>
-    <style>
+    <style scoped>
     .actors_item{
 
         list-style: none;
@@ -67,4 +101,11 @@ export default{
         flex-wrap: wrap;
 
     } 
+    a{
+
+        text-decoration: none;
+        color: black;
+        align-items: center;
+
+    }
 </style>
