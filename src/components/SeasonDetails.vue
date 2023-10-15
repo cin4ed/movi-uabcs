000da3e9e3bd4deed4444691c9e4842e079cea72
Mayerkk <eljorgemayer@gmail.com>
2023-10-14 21:47:27 -0700

    mejora del seasonsDetails
    se agrego imagen

--- src/components/SeasonDetails.vue
+++ src/components/SeasonDetails.vue
@@ -16,29 +16,42 @@
         numSeason:'',
         numCap:'',
         airDate:'',
+        serieUrl:[],
 
       };
 
     },
     mounted(){
-      app.axios.get('https://api.themoviedb.org/3/tv/91363?api_key=6a71a113dddd8d476e8b8e07db83bb9d&language=es-MX')
-                .then((resp)=>{
-                    //en caso de exito 
-                    console.log(resp)
-                    this.title=resp.data.name
-                    this.loading=false
-                    this.overviewer=resp.data.overview
-                    this.numSeason=resp.data.number_of_seasons
-                    this.airDate=resp.data.first_air_date
-                    this.numCap=resp.data.number_of_episodes
+      
+      app.axios.get('https://api.themoviedb.org/3/tv/91363?api_key=6a71a113dddd8d476e8b8e07db83bb9d')
+      .then((resp)=>{
+          //en caso de exito 
+          console.log(resp)
+          this.title=resp.data.name
+          this.loading=false
+          this.overviewer=resp.data.overview
+          this.numSeason=resp.data.number_of_seasons
+          this.airDate=resp.data.first_air_date
+          this.numCap=resp.data.number_of_episodes
 
-                })
-                .catch((error) => {
-                    // Aqui va el codigo para manejar los errores
-                    this.error = "Hubo un error al intentar cargar el catalogo de peliculas.";
-                    console.error("Error 404.");
-                    console.error(error);
-                })
+      })
+      .catch((error) => {
+          // Aqui va el codigo para manejar los errores
+          this.error = "Hubo un error al intentar cargar el catalogo de peliculas.";
+          console.error("Error 404.");
+          console.error(error);
+      })
+      app.axios.get('https://api.themoviedb.org/3/tv/91363/season/1/images?api_key=6a71a113dddd8d476e8b8e07db83bb9d')
+      .then((resp)=>{
+          this.serieUrl = resp.data.posters[0].file_path;
+          this.loading = false;
+          console.log(resp.data.posters[0].file_path)
+      })
+      .catch((error) => {
+          this.error = "Hubo un error al intentar cargar el catalogo de peliculas.";
+          console.error("Error 404.");
+          console.error(error);
+      })
     }
 
   }
@@ -48,8 +61,9 @@
   <div v-if="!loading">
     Seasons
     <h1>{{ title }}</h1>
+    <img :src="'https://www.themoviedb.org/t/p/w130_and_h195_bestv2/'+serieUrl">
     <p>{{ airDate }}</p>
-    <h3>{{ numSeason }}  {{ numCap }} Episodios</h3>
+    <h3>Temporada•{{ numSeason }}  {{ numCap }}•Episodios</h3>
     <h2>{{ overviewer }}</h2>
     
     
