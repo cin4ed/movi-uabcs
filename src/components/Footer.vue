33a1c66f82ddc823c5d8c96cc4a2c408ae382fd6
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-10-16 02:28:39 -0700

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="border-top mt-3">
+        <footer class="container d-flex flex-wrap justify-content-between align-items-center py-3">
+            <div class="col-md-4 d-flex align-items-center">
+                <a href="/" class="text-body-secondary text-decoration-none lh-1">
+                    <svg class="bi" width="30" height="24">
+                        <use xlink:href="#bootstrap"></use>
+                    </svg>
+                </a>
+                <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Company, Inc</span>
+            </div>
+            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex gap-3">
+                <li><a href="#" class="link-secondary"><i class="bi bi-twitter-x fs-3"></i></a></li>
+                <li><a href="#" class="link-secondary"><i class="bi bi-instagram fs-3"></i></a></li>
+                <li><a href="#" class="link-secondary"><i class="bi bi-facebook fs-3"></i></a></li>
+            </ul>
+        </footer>
+    </div>
+</template>
