f62b56b6d3931632f2e0bc67985722de8a7033e3
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-10-16 02:29:27 -0700

    Add Footer to app

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script>
   import Header from './components/Header.vue';
+  import Footer from './components/Footer.vue';
 
   export default { 
     name: "App",
     components: {
       Header,
+      Footer,
     },
   }
 </script>
 
 <template>
-  <Header></Header>
+  <Header />
   <main>
     <router-view></router-view>
   </main>
+  <Footer />
 </template>
-
